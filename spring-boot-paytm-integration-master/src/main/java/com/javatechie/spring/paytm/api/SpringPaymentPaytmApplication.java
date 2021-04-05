a98a46abaf810bdde67a4973a66380955e4a6ee6
Radhey <garoderadhey@gmail.com>
2021-04-05 11:32:08 +0530

    branch commite

--- spring-boot-paytm-integration-master/src/main/java/com/javatechie/spring/paytm/api/SpringPaymentPaytmApplication.java
+++ spring-boot-paytm-integration-master/src/main/java/com/javatechie/spring/paytm/api/SpringPaymentPaytmApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringPaymentPaytmApplication {
 
 	public static void main(String[] args) {
+		System.out.println("paytm3rdpartyapi");
 		SpringApplication.run(SpringPaymentPaytmApplication.class, args);
 	}
 
